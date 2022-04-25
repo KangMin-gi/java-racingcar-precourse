bb495fd2e1a746a9e0551c17e8cd1e22eff81345
kangmin-gi <bming0227@gmail.com>
2022-04-25 23:51:46 +0900

    refactor: CarFactoryTest 실패 해결
    CarName관련 valid를 Wrapper객체로 옮기면서 생기는 오류 해결

--- src/test/java/racingcar/car/CarFactoryTest.java
+++ src/test/java/racingcar/car/CarFactoryTest.java
@@ -4,10 +4,13 @@ import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import racingcar.racing.RacingCarNames;
 
 class CarFactoryTest {
 
@@ -17,7 +20,13 @@ class CarFactoryTest {
     @DisplayName("자동차 이름은 5자를 넘어가면 에러를 발생한다.")
     @ValueSource(strings = {"asdbdd", "dsadsad", "dsadsa", "dsadsad", "가나다라마사"})
     public void carNameLengthTest(String carName) {
-        assertThatThrownBy(() -> this.carFactory.createCar(new CarName(carName)))
+        assertThatThrownBy(() -> {
+            RacingCarNames racingCarNames = new RacingCarNames(Arrays.asList(carName));
+            List<CarName> carNames = racingCarNames.getCarNames();
+            for (CarName name : carNames) {
+                this.carFactory.createCar(name);
+            }
+        })
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR]");
     }
